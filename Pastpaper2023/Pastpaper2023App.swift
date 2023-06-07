e8eadf8749e9d8c255685852bd2526fb63e1c810
OOXXXX <zhupx2003@126.com>
2023-06-07 02:05:04 +0100

    Initial Commit

---
+++ Pastpaper2023/Pastpaper2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Pastpaper2023App.swift
+//  Pastpaper2023
+//
+//  Created by Patrick on 2023/6/7.
+//
+
+import SwiftUI
+
+@main
+struct Pastpaper2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
