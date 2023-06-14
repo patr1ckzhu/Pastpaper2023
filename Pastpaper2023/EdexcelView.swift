3c14127a4f3196ecf8523a3d56ab42473061e6f3
OOXXXX <zhupx2003@126.com>
2023-06-14 19:34:01 +0100

    添加了一门新的学科：edx-gcse-psychology

--- Pastpaper2023/EdexcelView.swift
+++ Pastpaper2023/EdexcelView.swift
@@ -93,7 +93,7 @@ struct EdexcelGCSEView: View {
                 NavigationLink(destination: YearListView(urlString: "http://13.41.199.9:8088/edx-gcse-physics", navTitle: "Physics Education")) {
                     Text("Physics Education")
                 }
-                NavigationLink(destination: Text("quali")) {
+                NavigationLink(destination: YearListView(urlString: "http://13.41.199.9:8089/edx-gcse-psychology", navTitle: "Psychology")) {
                     Text("Psychology")
                 }
                 NavigationLink(destination: Text("quali")) {
